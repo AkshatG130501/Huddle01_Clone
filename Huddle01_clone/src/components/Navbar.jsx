d6af24225ae1154e712d27edcdd164b1c131ebd5
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-14 19:59:32 +0530

    feat: Created Navbar

--- Huddle01_clone/src/components/Navbar.jsx
+++ Huddle01_clone/src/components/Navbar.jsx
@@ -4,21 +4,21 @@ const Navbar = () => {
   
 
   return (
-    <nav className='w-full bg-gray-950 shadow-lg flex items-center justify-between sticky top-0 border-b-[0.075rem] border-gray-100/50'>
+    <nav className='w-full bg-gray-950 shadow-lg flex items-center justify-between sticky top-0 border-b-[0.075rem] border-gray-100/20'>
         <div className='flex'>
           <div className='ml-28'>
             <img src={logo} alt="logo" className="w-40 h-16" />
           </div>
           <div className='flex space-x-7 ml-3'>
-            <button  className='text-white font-bold'>Platform</button>
-            <button className='text-white font-bold'>Developers</button>
-            <button className='text-white font-bold'>Network</button>
-            <button className='text-white font-bold'>Resources</button>
+            <button  className='text-white font-semibold'>Platform</button>
+            <button className='text-white font-semibold'>Developers</button>
+            <button className='text-white font-semibold'>Network</button>
+            <button className='text-white font-semibold'>Resources</button>
           </div>
         </div>
         <div className='flex mr-28 space-x-3'> 
-        <button className='w-[140px] h-[35px] font-bold text-white bg-blue-600 rounded-lg'>Start a Meeting</button>
-        <button className='w-[140px] h-[35px] bg-white font-bold rounded-lg'>Start Building</button>
+        <button className='w-[140px] h-[35px] font-semibold text-white bg-blue-600 rounded-lg'>Start a Meeting</button>
+        <button className='w-[140px] h-[35px] bg-white font-semibold rounded-lg'>Start Building</button>
         </div>
     </nav>
   );
